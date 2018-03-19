e5f7b42f224e4a1e12abf5961e3e1808a7f913d0
Tomasz Adamski <tadamski@redhat.com>
2018-03-19 19:25:18 +0100

    11/customer-gateway-hystrix

---
+++ chapter11/customer-gateway-hystrix/src/main/java/org/packt/swarm/petstore/proxy/PricingProxy.java
@@ -0,0 +1,43 @@
+package org.packt.swarm.petstore.proxy;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+import org.packt.swarm.petstore.pricing.api.Price;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@ApplicationScoped
+public class PricingProxy {
+
+    private final String targetPath = "http://pricing-service.petstore.svc:8080";
+
+    public Price getPrice(String itemId){
+        return new GetPriceCommand(itemId).execute().readEntity(Price.class);
+    }
+
+    private class GetPriceCommand extends HystrixCommand<Response> {
+
+        private final String itemId;
+
+        public GetPriceCommand(String itemId) {
+            super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("pricing-service"))
+                    .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+                            .withExecutionTimeoutInMilliseconds(100)));
+            this.itemId = itemId;
+        }
+
+        @Override
+        protected Response run() {
+            Client client = ClientBuilder.newClient();
+            WebTarget target = client.target(targetPath + "/price/" + itemId);
+            return target.request(MediaType.APPLICATION_JSON).get();
+        }
+    }
+}
+
